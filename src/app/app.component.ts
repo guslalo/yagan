b9c311602e9580ae71d81fe4e9d1ff6d20e90910
guslalo <gf.renjifo@gmail.com>
2018-05-01 15:58:58 -0400

    jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+import * as $ from 'jquery';
+//declare var $:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  ngOnInit() {
+    $(function () {
+      $('body [data-toggle="tooltip"]').tooltip()
+    }) 
+  }
 }
+
