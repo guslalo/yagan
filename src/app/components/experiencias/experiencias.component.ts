433d74bcab58d77ea3175efa64ff2f026dae6131
aconsulting <consulting.acb@gmail.com>
2019-03-02 02:58:25 -0300

    arreglo de filtro, de link de mapas

--- src/app/components/experiencias/experiencias.component.ts
+++ src/app/components/experiencias/experiencias.component.ts
@@ -25,12 +25,18 @@ export class ExperienciasComponent implements OnInit, OnChanges {
  duracion: Duracion[] = [];
  public resultados: subCategory;
 
+ cid_all: boolean;
+ rg_all: boolean;
+ dc_all: boolean;
+
  public query = '';
  localStorage: any;
 
  idCategoryActive: number;
  public categoryActive: Category = new Category;
  public banner: string;
+
+ params_url = new Map();
 
  id: any;
  private sub: any;
@@ -53,7 +59,92 @@ constructor( private http: HttpClient,
     console.log(JSON.parse(localStorage.getItem('resultados')));
   }
 
+  setUrl(code: any, value: any) {
+    this.params_url.set(code, value);
+    console.log(this.params_url);
+    this.getData2();
+    // this.idItem = id;
+  }
+
   ngOnInit() {
+    this.getData();
+  }
+
+  getData2() {
+    // get service
+    // this.route.params.subscribe(params => {
+      let paramSearch: string;
+      // let q: string;
+      let cid: string;
+      let rg: string;
+      let dc: string;
+
+      this.cid_all = true;
+      this.rg_all = true;
+      this.dc_all = true;
+
+      window.scrollTo(0, 493);
+
+      // q = (localStorage.getItem('q')=='') ? params['q'] : localStorage.getItem('q');
+      // cid = (localStorage.getItem('cid')=='') ? params['cid'] : localStorage.getItem('q');
+
+      // q =  (params['q']) ? params['q'] : '';
+      cid = (this.params_url.get('cid')) ? this.params_url.get('cid') : '';
+      rg = (this.params_url.get('rg')) ? this.params_url.get('rg') : '';
+      dc = (this.params_url.get('dc')) ? this.params_url.get('dc') : '';
+
+      // check region
+      for (const reg of this.region) {
+        reg.status = false;
+        if (rg === reg.code) {
+          reg.status = true;
+        }
+      }
+      // end check region
+      // check category
+      for (const cat of this.CategoryFilter) {
+        cat.status = false;
+        if (+cid === cat.id) {
+          cat.status = true;
+        }
+      }
+
+      // check duration
+      for (const d of this.duracion) {
+        d.status = false;
+        if (+dc === d.id) {
+          d.status = true;
+        }
+      }
+
+      paramSearch = '?';
+      paramSearch += 'q=';
+      if (cid !== '') {
+        paramSearch += '&category_parent=' + cid;
+        this.cid_all = false;
+      }
+      if (rg !== '') {
+        paramSearch += '&region=' + rg;
+        this.rg_all = false;
+      }
+      if (dc !== '') {
+        paramSearch += '&duration=' + dc;
+        this.dc_all = false;
+      }
+
+      console.log(paramSearch);
+      this.serviciosService.searchDefault(paramSearch).subscribe(
+        data => {
+          this.resultados = data;
+          // console.log(data);
+        },
+        error => {
+          console.log(<any>error);
+        }
+      );
+  }
+
+  getData() {
     this.mensaje = 'Este es el experiencias';
     this.banner = 'assets/img/cores/experiencias.png';
     this.titleService.setTitle('Rutas y experiencias | Yagan');
@@ -61,6 +152,9 @@ constructor( private http: HttpClient,
       $('.menusidebar').find('form').slideToggle();
     });
     window.scrollTo(0, 0);
+    this.cid_all = true;
+    this.rg_all = true;
+    this.dc_all = true;
 
     // get service
     this.route.params.subscribe(params => {
@@ -75,8 +169,19 @@ constructor( private http: HttpClient,
 
       q =  (params['q']) ? params['q'] : '';
       cid = (params['cid']) ? params['cid'] : '';
-      rg = (params['rg']) ? params['rg'] : '';
+      rg = (this.params_url.get('rg')) ? this.params_url.get('rg') : '';
       dc = (params['dc']) ? params['dc'] : '';
+
+      // check region
+
+      for (const reg of this.region) {
+        reg.status = false;
+        if (rg === reg.code) {
+          reg.status = true;
+        }
+      }
+      console.log(this.region);
+      // end check region
 
       paramSearch = '?';
 
@@ -86,14 +191,15 @@ constructor( private http: HttpClient,
       if (q !== '') {
         paramSearch += 'string_text=' + q;
       } else {
+        paramSearch += 'q=';
         if (cid !== '') {
-          paramSearch += 'category_parent=' + cid;
+          paramSearch += '&category_parent=' + cid;
         }
         if (rg !== '') {
-          paramSearch += 'region=' + rg;
+          paramSearch += '&region=' + rg;
         }
         if (dc !== '') {
-          paramSearch += 'duration=' + dc;
+          paramSearch += '&duration=' + dc;
         }
       }
       console.log(paramSearch);
@@ -129,6 +235,7 @@ constructor( private http: HttpClient,
     this.serviciosService.getRegiones().subscribe(
       data => {
         this.region = data;
+        console.log('get a region');
       },
       error => {
         console.log(<any>error);
